4e2da8ef78cc88d72ab842898dd99555f37e2243
Farouk <faroukbelaid9@gmail.com>
2025-10-14 22:55:15 +0300

    looping through list

---
+++ Lists/LoopingThroughList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingThroughList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Print list items through for loop.");
+
+            // for loop
+            for (int i = 0; i < list.Count; i++) 
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            Console.WriteLine("\n\nPrint list items through foreach.");
+
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n\nPrint list items using List foreach.");
+
+            list.ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
