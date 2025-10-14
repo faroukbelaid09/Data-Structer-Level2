8d2458e7f1e0da26060b936015eef2ec3d422607
Farouk <faroukbelaid9@gmail.com>
2025-10-14 19:10:45 +0300

    Working with lists

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+
+            Console.WriteLine("Number of items in the list: " + numbers.Count());
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[4]);
+
+            Console.WriteLine("Changing the value of first item to 500");
+
+            numbers[0] = 500;
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
