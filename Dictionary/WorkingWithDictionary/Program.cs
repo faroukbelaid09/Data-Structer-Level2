04997866716da32f5b8d23c38edc1be959b0d723
Farouk <faroukbelaid9@gmail.com>
2025-10-15 12:15:08 +0300

    Working with dictionary

---
+++ Dictionary/WorkingWithDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> fruits = new Dictionary<string, int>();
+
+            // Adding to a dictionary
+            fruits.Add("Banana",5);
+            fruits.Add("Lemon",3);
+            fruits.Add("Grapes",6);
+
+            // looping through dictionary
+
+            foreach (KeyValuePair<string,int> item in fruits) 
+            {
+                Console.WriteLine("Key: " + item.Key + " Value: " + item.Value);
+            }
+
+            // Removing item from dictionary
+            fruits.Remove("Lemon");
+
+            Console.WriteLine("\n--------------------\n");
+            // looping through dictionary
+            foreach (KeyValuePair<string, int> item in fruits)
+            {
+                Console.WriteLine("Key: " + item.Key + " Value: " + item.Value);
+            }
+        }
+    }
+}
