9ea8f8acb09ef9cd632ec6a796c256608a4c2d44
Farouk <faroukbelaid9@gmail.com>
2025-10-19 16:05:18 +0300

    Working with hashset

---
+++ HashSet/WorkingWithHashSet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> fruits = new HashSet<string>();
+
+            fruits.Add("Orange");
+            fruits.Add("Apple");
+            fruits.Add("Grapes");
+
+            // Trying to add duplicate value
+            fruits.Add("Orange");
+            fruits.Add("Orange");
+
+            // looping through the hashset
+            foreach (string fruit in fruits) 
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
