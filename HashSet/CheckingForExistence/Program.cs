78a5dc287f132d172adeea5f9bf3122364e95935
Farouk <faroukbelaid9@gmail.com>
2025-10-21 12:33:27 +0300

    Checking for existence in hashset

---
+++ HashSet/CheckingForExistence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingForExistence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> fruits = new HashSet<string>();
+
+            fruits.Add("Apple");
+            fruits.Add("Orange");
+            fruits.Add("Grapes");
+
+            if (fruits.Contains("Apple"))
+            {
+                Console.WriteLine("Apple!!!");
+            }
+            else
+            {
+                Console.WriteLine("Not a healthy day:(");
+            }
+        }
+    }
+}
