437d73c1cda2adbc361341131867493f594aaf74
Farouk <faroukbelaid9@gmail.com>
2025-10-21 12:36:09 +0300

    removing items from hashset

---
+++ HashSet/RemovingFromHashSet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovingFromHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> fruits = new HashSet<string>();
+
+            fruits.Add("Apple");
+            fruits.Add("Orange");
+            fruits.Add("Grapes");
+
+            Console.WriteLine("Count before: " + fruits.Count);
+
+            fruits.Remove("Orange");
+
+            Console.WriteLine("Count after: " + fruits.Count);
+        }
+    }
+}
