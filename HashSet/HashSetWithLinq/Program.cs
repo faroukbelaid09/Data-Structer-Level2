e85c8da5abb3f02ff291d6c0770d7db0d5e5cbb8
Farouk <faroukbelaid9@gmail.com>
2025-10-21 12:46:59 +0300

    using hashset with LINQ

---
+++ HashSet/HashSetWithLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetWithLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> nums = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evenNumbers = nums.Where(x => x%2 == 0);
+
+            foreach (int x in evenNumbers) 
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
