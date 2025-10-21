86756f6bf41efd2d4ee2064578410e789d714a3d
Farouk <faroukbelaid9@gmail.com>
2025-10-21 12:42:27 +0300

    Removing duplicate values using HashSet

---
+++ HashSet/RemoveDuplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+
+            HashSet<int> set = new HashSet<int>(arr);
+
+            foreach (int num in set) 
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
