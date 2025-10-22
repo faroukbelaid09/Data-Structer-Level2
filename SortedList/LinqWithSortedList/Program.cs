db90de889b92318495cddb74193a2a00d3439ed2
Farouk <faroukbelaid9@gmail.com>
2025-10-22 10:01:25 +0300

    using LINQ with sorted list

--- SortedList/LinqWithSortedList/Program.cs
+++ SortedList/LinqWithSortedList/Program.cs
@@ -45,6 +45,43 @@ namespace LinqWithSortedList
             {
                 Console.WriteLine("Length " + item.Key + ": " + string.Join(", ",item.Select(x=>x.Value)));
             }
+
+            Console.WriteLine("--------------------------------------------------\n");
+            // Sorted List with Custom Object
+
+            SortedList<int, Employees> employees = new SortedList<int, Employees>
+            {
+                {1, new Employees("Mark","HR",1000)},
+                {2, new Employees("Bob","IT",3000)},
+                {3, new Employees("Alice","HR",1000)},
+                {4, new Employees("Henry","Marketing",3000)},
+                {5, new Employees("Katya","IT",4000)},
+            };
+
+            var newEMPs = employees.Where(emp => emp.Value.Department == "IT")
+                .OrderByDescending(emp => emp.Value.salary)
+                .Select(emp => emp.Value.Name);
+
+            foreach(var employee in newEMPs)
+            {
+                Console.WriteLine(employee);
+            }
+
+        }
+    }
+
+    class Employees
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int salary { get; set; }
+
+
+        public Employees(string name, string department, int salary) 
+        {
+            this.Name = name;
+            this.Department = department;
+            this.salary = salary;
         }
     }
 }
